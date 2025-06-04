a4a933b94d18a9bc124c3380a26ceeef5b3b2d2f
Arturo Villalta <alvillalt@gmail.com>
2025-06-04 16:47:28 +0000

    Versión llamando a funciones

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
 import "bootstrap";
 import "./style.css";
 
+window.onload = function () {
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let whoRandom = who[randomNumber(who)];
+  let actionRandom = action[randomNumber(action)];
+  let whatRandom = what[randomNumber(what)];
+  let whenRandom = when[randomNumber(when)];
+
+  let textHtml = whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom;
+
+  const excuse = document.querySelector("#excuse");
+  excuse.innerHTML = textHtml;
+};
+
+function randomNumber(array) {
+  return Math.floor(Math.random() * array.length);
 };
